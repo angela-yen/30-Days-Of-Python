80e991d7b4762d9c77b62b7b457e8b6192497b24
angela-yen <angela.yen0210@gmail.com>
2024-07-11 22:50:59 +1200

    day 3

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -122,3 +122,133 @@ print(not True)      # False - Negation, the not operator turns true to false
 print(not False)     # True
 print(not not True)  # True
 print(not not False) # False
+
+# 1. Declare your age as integer variable
+age = 28
+
+# 2. Declare your height as a float variable
+height = 173.5
+
+# 3. Declare a variable that store a complex number
+complex_number = 1 + 1j
+
+# 4. Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+
+base = input('Enter base:' )
+height = input('Enter height: ')
+area = 0.5 * int(base) * int(height)
+print('The area of the triangle is ', area)
+
+    # Enter base: 20
+    # Enter height: 10
+    # The area of the triangle is 100
+
+# 5. Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+
+side_a = input('Enter side a: ')
+side_b = input('Enter side b: ')
+side_c = input('Enter side c: ')
+perimeter = int(side_a) + int(side_b) + int(side_c)
+print('The perimeter of the triangle is ', perimeter)
+
+# Enter side a: 5
+# Enter side b: 4
+# Enter side c: 3
+# The perimeter of the triangle is 12
+
+# 6. Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+
+length = input('Enter length: ')
+width = input('Enter width: ')
+area = int(length) * int(width)
+print('Area: ', area)
+ 
+primeter = 2 * int(length) * int(width)
+print('Perimeter: ', perimeter)
+
+# 7.Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+radius = input('Enter Radius: ')
+pi = 3.14
+area = pi * (int(radius) ** 2)
+circumference = 2 * pi * int(radius)
+print('Area ', area)
+print('Cicumference ', circumference)
+
+# 8. Calculate the slope, x-intercept and y-intercept of y = 2x -2
+slope_a = -2
+x_int = (0, -2)
+y_int = (1, 0)
+
+# 9. Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+slope_b = (10 - 2)/(6-2)
+distance = ((4**2) + (8**2)) * 0.5
+# 10. Compare the slopes in tasks 8 and 9.
+print(slope_a == slope_b)
+
+# 11. Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+
+# 12. Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+print(len('python') != len('dragon'))
+
+# 13. Use and operator to check if 'on' is found in both 'python' and 'dragon'
+print('on' in 'python')
+print('on' in 'dragon')
+
+print('on' in 'python' and 'on' in 'dragon')
+
+# 14. I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+print('jargon' in 'I hope this course is not full of jargon')
+
+# 15. There is no 'on' in both dragon and python
+print('on' not in 'dragon' and 'python')
+
+# 16. Find the length of the text python and convert the value to float and convert it to string
+to_float = float(len('python'))
+to_string = str(to_float)
+
+print(len('python'))
+print(float(len('python')))
+print(to_string)
+
+# 17. Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+x = int(input('Enter x:'))
+
+output = x % 2 == 0
+
+print('X is even', output)
+
+# 18. Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+print( 7 // 3 == int(2.7))
+
+# 19. Check if type of '10' is equal to type of 10
+print(type('10') == type(10))
+
+# 20. Check if int('9.8') is equal to 10
+print(int('9.8') == 10)
+
+# 21. Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+hours = input('Enter hours: ')
+rate = input('Enter rate per hour: ')
+print('Your weekly earning is: ', int(hours) * int(rate))
+
+# Enter hours: 40
+# Enter rate per hour: 28
+# Your weekly earning is 1120
+
+# 22. Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+
+years = input('Enter number of years you have lived: ')
+print('You have lived for ', int(years) * 31556952, 'seconds.')
+
+# Enter number of years you have lived: 100
+# You have lived for 3153600000 seconds.
+
+# 23. Write a Python script that displays the following table
+print([[1,1,1,1,1], [2,1,2,4,8], [3,1,4,9,27], [4,1,4,16,64], [5,1,5,25,125]])
+
+print(' 1 1 1 1\n 2 1 2 4 8\n 3 1 3 9 27\n 4 1 4 16 64\n 5 1 5 25 125')
+# 1 1 1 1 1
+# 2 1 2 4 8
+# 3 1 3 9 27
+# 4 1 4 16 64
+# 5 1 5 25 125
