2ebe02177ca82be2d3080c95c05b7e533d5ba56e
angela-yen <angela.yen0210@gmail.com>
2024-07-23 18:58:25 +1200

    day 5

--- 05_Day_Lists/day_5.py
+++ 05_Day_Lists/day_5.py
@@ -177,3 +177,174 @@ ages.sort()
 print(ages) 
 ages.sort(reverse=True)
 print(ages) 
+
+# Level 1
+
+# 1. Declare an empty list
+list = []
+
+# 2. Declare a list with more than 5 items
+list = ["apple", "banana", "orange", "grape", "lemon"]
+
+# 3. Find the length of your list
+len(list)
+
+# 4. Get the first item, the middle item and the last item of the list
+print(list[0])
+print(list[2])
+print(list[len(list) - 1])
+
+# 5. Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ["ange", 28, 174, "not married", "Salisbusry St"]
+
+# 6. Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+
+# 7. Print the list using print()
+print(it_companies)
+
+# 8. Print the number of companies in the list
+print(len(it_companies))
+
+# 9. Print the first, middle and last company
+print(it_companies[0])
+print(it_companies[2])
+print(it_companies[len(it_companies) - 1])
+
+print(it_companies[::3])
+
+# 10. Print the list after modifying one of the companies
+it_companies[0] = "Canva"
+print(it_companies)
+
+# 11. Add an IT company to it_companies
+it_companies.append('GitLab')
+
+# 12. Insert an IT company in the middle of the companies list
+it_companies.insert(4, 'Instagram')
+
+# 13. Change one of the it_companies names to uppercase (IBM excluded!)
+it_companies[2] = it_companies[2].upper()
+
+# 14. Join the it_companies with a string '#;  '
+new = it_companies + ['#;  ']
+print(new)
+
+# 15. Check if a certain company exists in the it_companies list.
+print('Instagram' in it_companies)
+
+# 16. Sort the list using sort() method
+it_companies.sort()
+
+# 17. Reverse the list in descending order using reverse() method
+it_companies.sort(reverse=True)
+
+# 18. Slice out the first 3 companies from the list
+del it_companies[0:3]
+
+# 19. Slice out the last 3 companies from the list
+del it_companies[-3::]
+
+# 20. Slice out the middle IT company or companies from the list
+middle = (len(it_companies) - 1 )/ 2
+it_companies.pop(middle)
+
+# 21. Remove the first IT company from the list
+it_companies.pop(0)
+
+# 22.Remove the middle IT company or companies from the list
+it_companies.pop(middle)
+
+# 23. Remove the last IT company from the list
+it_companies.pop
+
+# 24. Remove all IT companies from the list
+it_companies.clear
+
+# 25. Destroy the IT companies list
+del it_companies
+
+# 26. Join the following lists:
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+joined_list = front_end + back_end
+
+print(joined_list)
+
+# 27. After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack. Then insert Python and SQL after Redux.
+
+joined_list.insert(5, 'Python')
+joined_list.insert(6, 'SQL')
+
+full_stack = joined_list
+print(full_stack)
+
+# Level 2
+# The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+# 1. Sort the list and find the min and max age
+ages.sort()
+min = ages[0]
+max = ages[len(ages) -1]
+
+print(min)
+print(max)
+
+# 2. Add the min age and the max age again to the list
+ages.append(min)
+ages.append(max)
+
+print(ages)
+
+# 3. Find the median age (one middle item or two middle items divided by two)
+ages.sort()
+
+median_index = int((len(ages) - 1)/2)
+median_age = (ages[median_index] + ages[median_index + 1])/2
+
+print(median_age)
+
+# 4. Find the average age (sum of all items divided by their number )
+average_age = sum(ages)/len(ages)
+
+print(average_age)
+
+# 5. Find the range of the ages (max minus min)
+min = ages[0]
+max = ages[len(ages) - 1]
+range = max - min
+
+print(range)
+
+# 6. Compare the value of (min - average) and (max - average), use abs() method
+print((min - average_age)%(max - average_age))
+
+# 7. Find the middle country(ies) in the countries list
+countries = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+
+# Check if there is a middle 
+print(len(countries)%2)
+
+middle_index = int((len(countries) - 1)/2)
+
+middle_country = countries[middle_index]
+
+print(middle_country)
+
+# 8. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+list_1 = countries[0:3]
+list_2 = countries[3:len(countries)]
+
+print(list_1)
+print(list_2)
+
+# 9. Unpack the first three countries and the rest as scandic countries.
+first_country, second_country, third_country, *scandic = countries
+
+print(first_country)
+print(second_country)
+print(third_country)
+print(scandic)
